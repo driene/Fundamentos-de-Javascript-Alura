999616a419478aa12dab25bb5e4bd242ad4d1bc1
driene <driene@gmail.com>
2022-02-02 11:42:19 -0300

    truthy e falsy e conversoes

---
+++ conversoes.js
@@ -0,0 +1,20 @@
+// tipo de dado
+// booleanos
+
+//coneversao implicita
+const numero = 456;
+const numeroString = "456";
+
+// console.log(numero === numeroString)
+// console.log(numero == numeroString)
+// console.log(numero + numeroString)
+
+//conversao explicita
+
+//Number()
+console.log(numero + Number(numeroString))
+
+//String()
+console.log(String(numero) + numeroString)
+
+
