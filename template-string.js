955e5b02251056f8f452bd668448dacfd9136ca4
driene <driene@gmail.com>
2022-02-03 16:50:07 -0300

    comparações ternario e template strings

---
+++ template-string.js
@@ -0,0 +1,17 @@
+const nome = "Driene";
+const idade = 2021-1987;
+const cidadeDeNascimento = "João Monlevade";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " anos" + " e nasci na cidade de " + cidadeDeNascimento;
+//console.log(apresentacao)
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao)
+
+// const nome = "Leo";
+// const idade = 23;
+// const bebidaMaior = "cerveja";
+// const bebidaMenor = "suco";
+
+// const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+// console.log(pedido)
